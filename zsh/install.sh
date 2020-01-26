999c31de1fe2526dd5c4abb80ab7db3e8e54f59d
arohim <a.rohim.sama@gmail.com>
2020-01-26 19:23:55 +0700

    update git command

---
+++ zsh/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+source ./script/lib
+
+if test ! $(which zsh)
+then
+    info "Installing Oh-My-ZSH!"
+    brew install zsh
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    success "Oh-My-ZSH installed"
+else
+    success "skipped installing Oh-My-ZSH"
+fi
